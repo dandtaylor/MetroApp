dbf8b4c12999a9c9941c12d5dff5d827eec838e5
Dan <dandtaylor@gmail.com>
2017-04-19 21:02:44 +0000

    added bekeh plots

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
+import numpy as np
+import pickle
+
+from bokeh.charts import Histogram
+from bokeh.embed import components
+from bokeh.plotting import figure
 
 app = Flask(__name__)
+
+
+
+def histogram_figure_all():
+	df = pickle.load(open("static/bike_may2016.p", "r"))
+	TOOLS = 'box_zoom,crosshair,resize,reset'
+	hist = Histogram(df[df['Ride dist'] < 6], values='Ride dist', #color='End dist', 
+						toolbar_location='above', responsive=True, 
+						bins = 40, density=True, legend='top_right', tools=TOOLS, plot_width=700, plot_height=400)
+	script, div  = components(hist)
+	return script, div
+
+
+def histogram_figure():
+	df = pickle.load(open("static/bike_may2016.p", "r"))
+	TOOLS = 'box_zoom,crosshair,resize,reset'
+	hist = Histogram(df[df['Ride dist'] < 6], values='Ride dist', color='End dist', 
+						toolbar_location='above', responsive=True, 
+						bins = 40, density=True, legend='top_right', tools=TOOLS, plot_width=700, plot_height=400)
+	script, div  = components(hist)
+	return script, div
+
 
 
 @app.route('/')
@@ -9,7 +37,10 @@ def main():
 
 @app.route('/index')
 def index():
-  return render_template('index_cover.html')
+  script1, div1 = histogram_figure_all()
+  script2, div2 = histogram_figure()
+  return render_template('index_cover.html', plot_script1=script1, plot_div1=div1, 
+  						plot_script2=script2, plot_div2=div2)
 
 @app.route('/morning_map')
 def morning_map():
